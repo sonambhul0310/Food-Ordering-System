142387c662d2b58f3365ffc59c5291dae62cd957
[sonambhul0310] <[sonam.bhule9696@gmail.com]>
2024-09-01 19:17:33 +0530

    completed project

---
+++ frontend/src/components/layouts/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <footer>
+        <p className="text-center">
+            Food Delivery website - 2024-25. All Rights Reserved by WHO...
+        </p>
+    </footer>
+      
+    </>
+  )
+}
